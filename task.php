dbc66eca1ed1e51faec46716972e9a35a5853c61
waythin <wathinmarma@gmail.com>
2021-02-07 16:43:48 +0600

    021

---
+++ task.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <h4>
+        Registration
+    </h4>
+    <title>Lab Task</title>
+</head>
+<body>
+    <p>
+        
+    </p>
+    <pre>
+    <form>
+Name              :<input type="text" id="f" name="f"> <br>
+Email            :<input type="text" id="i" name="i"> <br>
+User Name         :<input type="text" id="p" name="p"> <br>
+Password          :<input type="password" id=a name=a><br>
+Confirm Password  :<input type="password" id=a name=a>
+
+Gender
+  <input type="radio" id="male" name="gender" value="male"><label for="male">Male</label><input type="radio" id="female" name="gender" value="female"><label for="female">Female</label><input type="radio" id="other" name="gender" value="other"><label for="other">Other</label>
+ 
+  <label for="birthday">Date of Birth:</label><br>
+  <input type="date" id="birthday" name="birthday">
+  <input type="submit">
+     
+        
+ <input type="reset" placeorder="RESET"><input type="submit" value="SUBMIT">
+
+
+    </form>
+</pre>
+</body>
+</html>
