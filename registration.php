2a47fd54b9f52f7ee7f077b5db7ae6b657d54042
waythin <wathinmarma@gmail.com>
2021-02-07 16:46:36 +0600

    lab task 01

---
+++ registration.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <h4>
+        Registration
+    </h4>
+    <title>Lab Task</title>
+</head>
+<body>
+    
+    <table>
+<tr>
+    <td>Name</td>
+    <td>:<input type="text" id="f" name="f"></td><br>
+    
+  </tr>
+  
+  <tr>
+    <td>Email</td>
+    <td>:<input type="text" id="i" name="i"></td><br>
+    
+  </tr>
+  <tr>
+    <td>User Name</td>
+    <td>:<input type="text" id="p" name="p"></td><br>
+  
+  </tr>
+  <tr>
+    <td>Password</td>
+    <td>:<input type="password" id=a name=a></td><br>
+  
+  </tr>
+  <tr>
+    <td>Confirm Password</td>
+    <td>:<input type="password" id=a name=a></td><br>
+  
+  </tr>
+  
+</table>
+
+Gender<br>
+  <input type="radio" id="male" name="gender" value="male">
+  <label for="male">Male</label><br>
+  <input type="radio" id="female" name="gender" value="female">
+  <label for="female">Female</label><br>
+  <input type="radio" id="other" name="gender" value="other">
+  <label for="other">Other</label><br>
+  <br>
+  <label for="birthday">Date of Birth:</label><br>
+  <input type="date" id="birthday" name="birthday"><br>
+  <input type="submit"><br>
+  <br>
+        
+
+ <input type="submit" value="SUBMIT">
+ <input type="reset" placeorder="RESET">
+
+
+</body>
+</html>
+
